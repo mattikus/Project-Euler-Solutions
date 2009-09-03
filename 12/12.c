dc7ed713c25c38df8d2a0f917f15389ccad6005b
Matt Kemp <matt@mattikus.com>
2009-09-03 15:02:43 -0500

    Added problem 12 in C

---
+++ 12/12.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int numdiv(long x)
+{
+  int divs = 2; // 1 and x
+  int root = (int)sqrt(x) + 1;
+  for (int i = 2; i <= root; i++) {
+    if (x % i == 0) {
+      divs += 2; // i and x / i
+    }
+  }
+  return divs;
+}
+
+int main()
+{
+  long answer = 0;
+
+  for (long i=1; ;i++) {
+    answer += i;
+    if (numdiv(answer) > 500) {
+      break;
+    }
+  }
+  printf("Problem 12 Answer: %ld\n", answer);
+  return 0;
+}
