8b3a72472c8e97531a216a91dcdfa7b281377bd4
Matt Kemp <matt@mattikus.com>
2009-09-03 15:05:24 -0500

    Changed compiler to clang.  Updated makefiles. Added problem 14 in C and 16 in py.

---
+++ 14/14.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+long doseq(long n)
+{
+  int steps = 1;
+
+  while (n > 1) {
+    if (n % 2 == 0) {
+      n /= 2;
+    } else {
+      n = (n * 3) + 1;
+    }
+
+    steps++;
+  }
+  return steps;
+}
+
+int main()
+{
+  long answer = 0;
+  long tmp;
+  long oldtmp = 0;
+
+  for (long i=1; i < 1000000; i++) {
+    tmp = doseq(i);
+    if (tmp > oldtmp) {
+      answer = i;
+      oldtmp = tmp;
+    }
+  }
+  printf("Problem 14 Answer: %ld\n", answer);
+  return 0;
+}
---
+++ 16/16.py
@@ -0,0 +1,1 @@
+print 'Problem 16 Answer:', sum(int(x) for x in str(2**1000))
