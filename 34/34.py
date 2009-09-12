1c1f47018368db353174d16aacab2aac4da48054
Matt Kemp <matt@mattikus.com>
2009-09-12 13:53:49 -0500

    Cleaned up code a bit, rather pointlessly.

--- 34/34.py
+++ 34/34.py
@@ -2,8 +2,5 @@
 
 from math import factorial
 
-def facmagic(x):
-  return x == sum(factorial(int(y)) for y in str(x))
-
+def facmagic(x): return x == sum(factorial(int(y)) for y in str(x))
 print 'Problem 34 Answer:', sum(x for x in xrange(3,100000) if facmagic(x))
-
