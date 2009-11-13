9daa39deb271cdb7be1f2ebd6101b73945688f38
Matt Kemp <matt@mattikus.com>
2009-11-13 15:50:17 -0600

    added go for 5, updated makefiles yet again.

---
+++ 5/5.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+  "fmt";
+)
+
+func isdiv(i int) bool {
+  for j := 2; j < 21; j++ {
+    if i % j != 0 {
+      return false
+    }
+  }
+  return true
+}
+
+func main() {
+  answer := 2520;
+  for ! isdiv(answer) {
+    answer += 20
+  }
+  fmt.Println("Problem 5 Answer:", answer)
+}
