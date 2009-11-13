22014e048c6ac72a84df4628aaf4debebc797b44
Matt Kemp <matt@mattikus.com>
2009-11-13 15:17:33 -0600

    Added go solution for problem 4.

---
+++ 4/4.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "fmt";
+  "strconv";
+)
+
+func ispalin(x int) bool {
+  s := strconv.Itoa(x);
+  slen := len(s);
+  for i := 0; i < (slen / 2); i++ {
+    if ! (s[i] == s[(slen-1)-i]) {
+      return false
+    }
+  }
+  return true
+}
+
+func main() {
+  var answer int;
+  for i := 100; i < 1000; i++ {
+    for j := 999; j > 99; j-- {
+      tmp := i * j;
+      if ispalin(tmp) && tmp > answer {
+        answer = tmp
+      }
+    }
+  }
+  fmt.Println("Problem 4 Answer:", answer)
+}
