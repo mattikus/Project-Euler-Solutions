3a2c4ab3785d67acfda385a7da48bb15c2b1fc84
Matt Kemp <matt@mattikus.com>
2009-12-08 01:32:14 -0600

    Refined new project mechanism, added some project templates

--- new_proj.sh
+++ new_proj.sh
@@ -1,6 +1,29 @@
+#!/bin/bash
+
+function tmpl() {
+  tmpl=$1
+  name=$2
+  ext=$(echo $tmpl | sed 's/templates\/\(.*\)\.tmpl/\1/')
+
+  if [[ $ext = "Makefile" ]]; then 
+    out="$name/Makefile"
+  else
+    out="$name/$name.$ext"
+  fi
+
+  sed "s/##/$2/g" $tmpl > $out
+}
+
 if [ -z $1 ]; then
   echo Requires One Argument
 else
-  mkdir -p $1 && sed "s/<>/$1/g" Makefile.tmpl > ./$1/Makefile  
-  touch $1/$1.py $1/$1.c $1/$1.go
+  if [[ -d $1 ]]; then
+    echo "Problem $1 already exists... Bailing."
+    exit 1
+  else
+    mkdir -p $1 
+    for i in templates/*.tmpl; do
+      tmpl $i $1
+    done
+  fi
 fi
